d93b9a2e9daff764a1873de96c5e7023af3a43db
DrewM1425 <dmeaux04@gmail.com>
2020-04-02 15:20:21 -0400

    Sorting things out

--- behindTheTable.js
+++ behindTheTable.js
@@ -1,14 +1,103 @@
 
-var calcMean = function(penguin) //Calculates the mean given a penguin
+var quizMean = function(penguin)
 {
+    return d3.mean(penguin.quizes.map(function(quiz) { return quiz.grade;}));
+}
+
+var hwMean = function(penguin)
+{
+    return d3.mean(penguin.homework.map(function(hw) { return hw.grade;}));
+}
+
+var testMean = function(penguin)
+{
+    return d3.mean(penguin.test.map(function(test) { return test.grade;}));
+}
+
+var getFinal = function(penguin)
+{
+    return penguin.final[0];
+}
+
+var clearTable = function()
+{
+    d3.selectAll("#tb tobdy tr")
+      .remove();
     
 }
 
 
+
+
+var initHeaders = function(penguins) //Adds interactivity 
+{
+    d3.select("#quizzes")
+    .on("click",function()
+    { console.log("clicked");
+        penguins.sort(function(a,b)
+        {
+            aMean = quizMean(a);
+            bMean = quizMean(b);
+            if(aMean > bMean) {return 1}
+            else if(aMean < bMean) {return -1}
+            else { return 0;}
+            
+        });
+        console.log(penguins);
+        clearTable();
+        createTable(penguins);
+    });
+    
+    d3.select("#homework")
+    .on("click",function()
+    { console.log("clicked");
+        penguins.sort(function(a,b)
+        {
+            aMean = hwMean(a);
+            bMean = hwMean(b);
+            if(aMean > bMean) {return 1}
+            else if(aMean < bMean) {return -1}
+            else { return 0;}
+        });
+        clearTable();
+        createTable(penguins);
+    });
+    
+    d3.select("#tests")
+    .on("click",function()
+    { console.log("clicked");
+        penguins.sort(function(a,b)
+        {
+            aMean = testMean(a);
+            bMean = testMean(b);
+            if(aMean > bMean) {return 1}
+            else if(aMean < bMean) {return -1}
+            else { return 0;}
+        });
+        clearTable();
+        createTable(penguins);
+    });
+    
+    d3.select("#finals")
+    .on("click",function()
+    { console.log("clicked");
+        penguins.sort(function(a,b)
+        {
+            aFinal = getFinal(a);
+            bFinal = getFinal(b);
+            if(aFinal > bFinal) {return 1}
+            else if(aFinal < bFinal) {return -1}
+            else { return 0;}
+        });
+        clearTable();
+        createTable(penguins);
+    });
+}
+
 var createTable = function(penguins)
 {
     //Creates a row for each penguin
-    var rows = d3.select("table")
+    var rows = d3.select("tbody")
       .selectAll("tr")
         .data(penguins)
         .enter()
@@ -37,7 +126,9 @@ var createTable = function(penguins)
     //append mean test scores
     rows.append("td")
         .text(function(penguin){return penguin.final.map(function(final)
+       
                                                         { return final.grade;});});
+    initHeaders(penguins);
 }
 
 
@@ -48,6 +139,7 @@ var successFcn = function(penguins) //If the data is successfully collected
 {
     console.log("Data Collected:",penguins);
     createTable(penguins);
+    initHeaders(penguins);
 }
 
 var failureFcn = function(errorMsg) //If there was an error
